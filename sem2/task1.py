15c4f0a2a6809781da5301d5168a35f74d737cbd
Dmitriy <seriousdim@mail.ru>
2023-09-16 13:11:31 +0300

    add sem2/task1

---
+++ sem2/task1.py
@@ -0,0 +1,27 @@
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное
+
+#  строковое представление. Функцию hex используйте для проверки своего результата.
+
+
+number = int(input("введите число: "))
+result = ""
+
+
+while number > 0:
+    if number % 16 < 10:
+        result = str(number % 16) + result
+    elif number % 16 == 10:
+        result = "a"+result
+    elif number % 16 == 11:
+        result = "b"+result
+    elif number % 16 == 12:
+        result = "c"+result
+    elif number % 16 == 13:
+        result = "d"+result
+    elif number % 16 == 14:
+        result = "e"+result
+    elif number % 16 == 15:
+        result = "f"+result
+    number //= 16
+
+print(result)
